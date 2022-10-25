c186ad7f537f73799dbd7f389e0053ea79d01374
jlehenbauer <L00170184@atu.ie>
2022-10-25 21:07:01 +0100

    initial files added

---
+++ src/main/java/actionsDemo/numbers.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package actionsDemo;
+
+/**
+ * @author jayne
+ *
+ */
+public class numbers {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hello");
+	}
+
+}
