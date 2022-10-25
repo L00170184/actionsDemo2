8a49a2ea425fba1bec8a403c7bd66a2401c24209
L00170184 <113636344+L00170184@users.noreply.github.com>
2022-10-25 21:39:08 +0100

    Update numbers.java
    testing actions - added 'hello world'

--- src/main/java/actionsDemo/numbers.java
+++ src/main/java/actionsDemo/numbers.java
@@ -7,29 +7,10 @@ package actionsDemo;
  * @author jayne
  *
  */
-class For_Each     
+class Numbers    
 {
     public static void main(String[] arg)
     {
-        {
-            int[] marks = { 125, 132, 95, 116, 110 };
-              
-            int highest_marks = maximum(marks);
-            System.out.println("The highest score is " + highest_marks);
-        }
-    }
-    public static int maximum(int[] numbers)
-    { 
-        int maxSoFar = numbers[0];
-          
-        // for each loop
-        for (int num : numbers) 
-        {
-            if (num > maxSoFar)
-            {
-                maxSoFar = num;
-            }
-        }
-    return maxSoFar;
+        System.out.print("Hello");
     }
 }
