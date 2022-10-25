87aa9dab5ee0ef0c060f2d70d47348ad22d6c9af
L00170184 <113636344+L00170184@users.noreply.github.com>
2022-10-25 21:28:00 +0100

    Update numbers.java
    added 'world'

--- src/main/java/actionsDemo/numbers.java
+++ src/main/java/actionsDemo/numbers.java
@@ -14,7 +14,7 @@ public class numbers {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("Hello");
+		System.out.print("Hello world");
 	}
 
 }
